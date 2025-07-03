697478b8831770e0a80643c8ed6d077d78e6f97b
beastwood12 <62308969+beastwood12@users.noreply.github.com>
2025-07-03 10:33:17 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
   },
-  base: './',
+  base: './'
 })
